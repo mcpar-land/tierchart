248ee5a01f790f7a2f72b8e0070382bc9033aba7
mcpar-land <john@mcpar.land>
2025-09-09 22:29:59 -0400

    add ggst

--- src/games.ts
+++ src/games.ts
@@ -1,12 +1,12 @@
 import type { GameData } from "./types";
 import tekken8 from './assets/tekken.json';
 import sf6 from './assets/sf6.json';
-// import ggst from './assets/ggst.json';
+import ggst from './assets/ggst.json';
 
 const gameDefinitions: GameData[] = [
   tekken8,
   sf6,
-  // ggst
+  ggst
 ];
 
 export default gameDefinitions
