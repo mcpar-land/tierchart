8cc5d43cdf50aee5ac414990a5e97039b07ef55e
mcpar-land <john@mcpar.land>
2025-09-09 22:21:49 -0400

    add sf6

--- src/games.ts
+++ src/games.ts
@@ -1,9 +1,11 @@
 import type { GameData } from "./types";
 import tekken8 from './assets/tekken.json';
+import sf6 from './assets/sf6.json';
 // import ggst from './assets/ggst.json';
 
 const gameDefinitions: GameData[] = [
   tekken8,
+  sf6,
   // ggst
 ];
 
